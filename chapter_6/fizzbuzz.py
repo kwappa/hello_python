0a3abaca3b2b11a474c9a24e4182e2619960fc3d
SHIOYA, Hiromu <kwappa.856@gmail.com>
2020-05-23 15:47:13 +0900

    P.85 Reverse checking order

--- chapter_6/fizzbuzz.py
+++ chapter_6/fizzbuzz.py
@@ -1,9 +1,9 @@
 for i in range(1, 51):
-    if i % 3 == 0:
-        print ('フィズ')
+    if i % 15 == 0:
+        print ('フィズバズ')
     elif i % 5 == 0:
         print ('バズ')
-    elif i % 15 == 0:
-        print ('フィズバズ')
+    elif i % 3 == 0:
+        print ('フィズ')
     else:
         print (i)
