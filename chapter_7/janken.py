17ab88fee6f395256afc579c2e6d5688bf0bd81d
SHIOYA, Hiromu <kwappa.856@gmail.com>
2020-05-24 11:29:05 +0900

    P.127 Add comment

--- chapter_7/janken.py
+++ chapter_7/janken.py
@@ -1,11 +1,16 @@
+# 変数の宣言
 num = 1
 you = 0
 key = ""
+
+# 10回繰り返す
 while num <= 10:
     print (num, "回目")
     print ("あなたが出すのは？(「1=グー 2=チョキ \
 3=パー」の後にEnterを押す)")
+# 文字の入力
     key = input()
+# 文字が1、2、3だった場合
     if (key == "1" or key == "2" or key == "3"):
         you = int(key)
         print (you)
