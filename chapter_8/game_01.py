48bdbbea28f254ccb5b36465802c99e375e20a06
SHIOYA, Hiromu <kwappa.856@gmail.com>
2020-05-24 14:18:28 +0900

    P.169 Add Activity Diagram of squash game

--- chapter_8/game_01.py
+++ chapter_8/game_01.py
@@ -1,6 +1,7 @@
-# 全体のプログラム
+# スカッシュゲーム（壁打ちテニス）
 # モジュールのインポート
 from tkinter import *
+import random
 
 # ウィンドウの作成
 win = Tk()
@@ -8,11 +9,21 @@ cv = Canvas(win, width = 640, height = 480)
 cv.pack()
 
 # ゲームの初期化
-ball_ichi_x = 300
-ball_ichi_y = 150
-ball_idou_x =  30
-ball_idou_y = -30
-ball_size = 10
+def init_game():
+    global is_gameover, ball_ichi_x, ball_ichi_y
+    global ball_idou_x, ball_idou_y, ball_size
+    global racket_ichi_x, racket_size, point, speed
+    is_gameover = False
+    ball_ichi_x = 0
+    ball_ichi_y = 250
+    ball_idou_x = 15
+    ball_idou_y = -15
+    ball_size = 10
+    racket_ichi_x = 0
+    racket_size = 100
+    point = 0
+    speed = 50
+    win.title("スカッシュゲーム : スタート！")
 
 # 画面の描画
 def draw_screen():
@@ -23,23 +34,39 @@ def draw_screen():
 
 # ボールを描く
 def draw_ball():
-    global ball_ichi_x, ball_ichi_y, ball_idou_x, ball_idou_y
-
     cv.create_oval(ball_ichi_x - ball_size, ball_ichi_y - ball_size,
         ball_ichi_x + ball_size, ball_ichi_y + ball_size,
         fill = "red")
 
+# ラケットを描く
+def draw_racket():
+    cv.create_rectangle(racket_ichi_x, 470, racket_ichi_x + racket_size, 480, fill = "yellow")
+
 # ボールの移動
 def move_ball():
-    global ball_ichi_x, ball_ichi_y, ball_idou_x, ball_idou_y
+    global is_gameover, point, ball_ichi_x, ball_ichi_y, ball_idou_x, ball_idou_y
+    if is_gameover: return
 
     # 左右の壁に当たったかの判定
     if (ball_ichi_x + ball_idou_x < 0 or ball_ichi_x + ball_idou_x > 640):
         ball_idou_x *= -1
 
-    # 天井か床に当たったかの判定
-    if (ball_ichi_y + ball_idou_y < 0 or ball_ichi_y + ball_idou_y > 480):
+    # 天井に当たったかの判定
+    if (ball_ichi_y + ball_idou_y < 0):
         ball_idou_y *= -1
+
+    # ラケットに当たったかの判定
+    if (ball_ichi_y + ball_idou_y > 470 and (racket_ichi_x <= (ball_ichi_x + ball_idou_x) <= (racket_ichi_x + racket_size))):
+        ball_idou_y *= -1
+        if (random.randint(0,1) == 0):
+            ball_idou_x *= -1
+        point += 10
+        win.title("得点 = " + str(point))
+
+    # ミスしたときの判定
+    if (ball_ichi_y + ball_idou_y >= 480):
+        is_gameover = True
+        win.title("得点 = " + str(point))
 
     # ボールの位置を移動
     if (0 <= ball_ichi_x + ball_idou_x <= 640):
@@ -47,13 +74,28 @@ def move_ball():
     if (0 <= ball_ichi_y + ball_idou_y <= 480):
         ball_ichi_y = ball_ichi_y + ball_idou_y
 
+# マウスの動きの処理
+def motion(event):              # マウスポインタの位置確認
+    global racket_ichi_x
+    racket_ichi_x = event.x
+
+def click(event):               # クリックで再スタート
+    if event.num == 1:
+        init_game()
+
+# マウスの動きとクリックの確認
+win.bind('<Motion>', motion)
+win.bind('<Button>', click)
+
 # ゲームの繰り返し処理の指令
 def game_loop():
     draw_screen()
     draw_ball()
+    draw_racket()
     move_ball()
-    win.after(50, game_loop)
+    win.after(speed, game_loop)
 
 # ゲームのメイン処理
+init_game()
 game_loop()
 win.mainloop()
