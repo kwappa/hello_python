8d9f1ef8382bfd141a89ac09c4df8dbbc223cf7b
SHIOYA, Hiromu <kwappa.856@gmail.com>
2020-05-24 11:27:39 +0900

    P.126 Fix input checking

--- chapter_7/janken.py
+++ chapter_7/janken.py
@@ -1,11 +1,14 @@
 num = 1
+you = 0
+key = ""
 while num <= 10:
     print (num, "回目")
     print ("あなたが出すのは？(「1=グー 2=チョキ \
 3=パー」の後にEnterを押す)")
     key = input()
-    if (key == 1 or key == 2 or key == 3):
-        print (key)
+    if (key == "1" or key == "2" or key == "3"):
+        you = int(key)
+        print (you)
         num += 1
     else:
         print ("1〜3の数字を入力してください！")
