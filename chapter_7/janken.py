249d3762cf007a9e027ff8060efeb51b6313a105
SHIOYA, Hiromu <kwappa.856@gmail.com>
2020-05-24 11:25:21 +0900

    P.124 Check input by wrong way

--- chapter_7/janken.py
+++ chapter_7/janken.py
@@ -4,6 +4,8 @@ while num <= 10:
     print ("あなたが出すのは？(「1=グー 2=チョキ \
 3=パー」の後にEnterを押す)")
     key = input()
-    print (key)
-
-    num += 1
+    if (key == 1 or key == 2 or key == 3):
+        print (key)
+        num += 1
+    else:
+        print ("1〜3の数字を入力してください！")
